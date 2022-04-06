a5ea5a604fdfe2f21825b753fdfa2e59edf03de3
Ankit Mishra <1507256+mishra-ankit@users.noreply.github.com>
2022-04-06 08:39:15 +0200

    attempt 2

--- sw.js
+++ sw.js
@@ -1,20 +1,19 @@
 console.log('Hello from sw.js');
+self.addEventListener('fetch', (event) => {
+    // intercept all requests to *.vscode-unpkg.net and send to CORS proxy
+    const url = new URL(event.request.url);
+    const list = ['vscode-unpkg.net', 'code.visualstudio.com'];
+    if (list.some(x => url.hostname.endsWith(x))) {
+        console.log('intercepting request', event.request.url);
+        event.respondWith(
+            fetch('https://ncert.centralindia.cloudapp.azure.com:8080/' + event.request.url)
+        );
+    }
+});
 
 self.addEventListener("install", event => {
     self.skipWaiting();
 
-
-    self.addEventListener('fetch', (event) => {
-        // intercept all requests to *.vscode-unpkg.net and send to CORS proxy
-        const url = new URL(event.request.url);
-        const list = ['vscode-unpkg.net', 'code.visualstudio.com'];
-        if (list.some(x => url.hostname.endsWith(x))) {
-            console.log('intercepting request', event.request.url);
-            event.respondWith(
-                fetch('https://ncert.centralindia.cloudapp.azure.com:8080/' + event.request.url)
-            );
-        }
-    });
 
     console.log("Service worker installed");
 });
