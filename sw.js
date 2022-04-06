b24c8ae36c75dc215aa5cbfe0e18ee4dcc30c1f5
Ankit Mishra <1507256+mishra-ankit@users.noreply.github.com>
2022-04-06 08:31:32 +0200

    attempt 1 - fix SW intercept

--- sw.js
+++ sw.js
@@ -2,20 +2,22 @@ console.log('Hello from sw.js');
 
 self.addEventListener("install", event => {
     self.skipWaiting();
+
+
+    self.addEventListener('fetch', (event) => {
+        // intercept all requests to *.vscode-unpkg.net and send to CORS proxy
+        const url = new URL(event.request.url);
+        const list = ['vscode-unpkg.net', 'code.visualstudio.com'];
+        if (list.some(x => url.hostname.endsWith(x))) {
+            console.log('intercepting request', event.request.url);
+            event.respondWith(
+                fetch('https://ncert.centralindia.cloudapp.azure.com:8080/' + event.request.url)
+            );
+        }
+    });
+
     console.log("Service worker installed");
 });
 self.addEventListener("activate", event => {
     console.log("Service worker activated");
-});
-
-self.addEventListener('fetch', (event) => {
-    // intercept all requests to *.vscode-unpkg.net and send to CORS proxy
-    const url = new URL(event.request.url);
-    const list = ['vscode-unpkg.net', 'code.visualstudio.com'];
-    if (list.some(x => url.hostname.endsWith(x))) {
-        console.log('intercepting request', event.request.url);
-        event.respondWith(
-            fetch('https://ncert.centralindia.cloudapp.azure.com:8080/' + event.request.url)
-        );
-    }
 });
