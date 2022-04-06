324bc13303d5c52f5ef6d2a1d16de28808e5ee90
Ankit Mishra <1507256+mishra-ankit@users.noreply.github.com>
2022-04-06 22:44:53 +0200

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   ignorePatterns: ['**/*.d.ts', '**/*.test.ts', '**/*.js', 'sample/**/*.*'],
   parser: '@typescript-eslint/parser',
   extends: ['plugin:@typescript-eslint/recommended'],
-  plugins: ['header'],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
@@ -14,14 +13,6 @@ module.exports = {
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
-    'header/header': [
-      'error',
-      'block',
-      `---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------`,
-    ],
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
--- src/server/download.ts
+++ src/server/download.ts
@@ -1,8 +1,3 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 import { promises as fs, existsSync, createWriteStream } from 'fs';
 import * as path from 'path';
 
@@ -14,14 +9,18 @@ import { URL } from 'url';
 
 import * as decompress from 'decompress';
 import * as decompressTargz from 'decompress-targz';
-import { Static } from './main';
-
+interface Static {
+	readonly type: 'static';
+	readonly location: string;
+	readonly quality: 'stable' | 'insider';
+	readonly version: string;
+}
 interface DownloadInfo {
 	url: string;
 	version: string;
 }
 
-const folderName = `vscode-web-stable`;
+const folderName = `public`;
 export const downloadedPath = path.resolve( process.cwd(), folderName);
 
 async function getLatestVersion(quality: 'stable' | 'insider'): Promise<DownloadInfo> {
