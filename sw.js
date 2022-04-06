12a5dd1a431c7e79545a29fb17037183a751926d
Ankit Mishra <1507256+mishra-ankit@users.noreply.github.com>
2022-04-06 08:22:34 +0200

    fix sw to include more domains

--- sw.js
+++ sw.js
@@ -2,17 +2,19 @@ console.log('Hello from sw.js');
 
 self.addEventListener("install", event => {
     console.log("Service worker installed");
- });
- self.addEventListener("activate", event => {
+});
+self.addEventListener("activate", event => {
     console.log("Service worker activated");
- });
+});
 
- self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', (event) => {
     // intercept all requests to *.vscode-unpkg.net and send to CORS proxy
-    if (event.request.url.indexOf('vscode-unpkg.net') > 0) {
-        console.log('intercepting request to vscode-unpkg.net');
+    const url = new URL(event.request.url);
+    const list = ['vscode-unpkg.net', 'code.visualstudio.com'];
+    if (list.some(x => url.hostname.endsWith(x))) {
+        console.log('intercepting request', event.request.url);
         event.respondWith(
             fetch('https://ncert.centralindia.cloudapp.azure.com:8080/' + event.request.url)
         );
     }
-  });
+});
